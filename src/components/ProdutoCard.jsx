f709dd5efbc034f8d7c9a07f2a2beaafa69a26b9
Gus <gfavero2005@gmail.com>
2025-06-16 21:06:52 -0300

    Troubleshooting

--- src/components/ProdutoCard.jsx
+++ src/components/ProdutoCard.jsx
@@ -1,10 +1,19 @@
 import React from "react"
 import "../styles/ProdutoCard.css"
+import camisa from '../assets/camisa.jpeg';
+import jeans from '../assets/jeans.jpeg';
+import tenis from '../assets/tenis.jpeg';
+
+const imagens = {
+  'camisa.jpeg': camisa,
+  'jeans.jpeg': jeans,
+  'tenis.jpeg': tenis,
+};
 
 export default function ProdutoCard({ produto, adicionarAoCarrinho}) {
     return(
         <div className="produto-card">
-            <img src={produto.imagem} alt={produto.nome}/>
+            <img src={imagens[produto.imagem]} alt={produto.nome}/>
             <h2>{produto.nome}</h2>
             <p>R$ {produto.preco.toFixed(2)}</p>
             <button onClick={() => adicionarAoCarrinho(produto)}>Adicionar</button>
